289c8290651f2dcae3eb12108b3490ae4297bcbe
Oliver Torres <olivertorres1997@gmail.com>
2023-11-11 20:53:12 -0400

    fastify actualizacion

--- front/src/components/LectorEntradaSA.jsx
+++ front/src/components/LectorEntradaSA.jsx
@@ -33,7 +33,7 @@ function LectorEntradaSA({ingresos,setIngresos,headers}) {
         nombre: message.Tarjetum ? message.Tarjetum.Usuario.nombres +', '+ message.Tarjetum.Usuario.apellidos : 'DESCONOCIDO',
         carrera: message.Carrera ? message.Carrera.nombre : 'DESCONOCIDO',
         tipo: message.Tipo ? message.Tipo.nombre.toUpperCase() : '',
-        avatar:message.Tarjetum ? message.Tarjetum.Usuario.avatar : avatar,
+        avatar:message.Tarjetum ? (message.Tarjetum.Usuario.avatar ?? avatar) : avatar,
         estatus:message.estatus == 1 ? "PASO" : (message.estatus == 2 ? "NO PASO" : "RECHAZADO")
       });
     }
